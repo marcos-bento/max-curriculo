01d29fd0bbd90951a3dbc9304f0e09294a23ee81
Marcos Bento <marcos.x.marcos@hotmail.com>
2025-05-19 21:50:59 -0300

    Commit 19-05

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import Form from './pages/Form';
-import Preview from './pages/Preview';
+import Home from './pages/Home/Home';
+import Form from './pages/Form/Form';
+import Preview from './pages/Preview/Preview';
 
 function App() {
   return (
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Navbar from '../../components/Navbar/Navbar';
 
 function Home() {
   return (
     <div>
+      <Navbar/>
       <h1>Bem-vindo ao MaxCurriculo</h1>
       <Link to="/form">Criar Currículo</Link>
     </div>
